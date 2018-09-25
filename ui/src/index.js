a41db5b46d707e7b2b50d30ee18403fd5137e7c9
wjase <wjase@yahoo.com.au>
2018-09-25 12:22:58 +1000

    updated pre redux

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,7 +3,28 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+
+//state
+//import { createStore, applyMiddleware } from 'redux';
+//import thunk from 'redux-thunk';
+//import { Provider } from 'react-redux';
+//import rootReducer from './reducers';
+
+
+// const store = createStore(
+//     rootReducer,
+//     applyMiddleware(thunk)
+//   );
+
+//store.dispatch(fetchAsyncDataRequest());
+
+store.dispatch(fetchHeats());
+
+// ReactDOM.render(
+// <Provider store={store}>
+//     <App />
+//   </Provider>, document.getElementById('root'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
